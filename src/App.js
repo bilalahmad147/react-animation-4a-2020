4412c04eed02bd16359fe1cbaf290e38d8dff1ab
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-04-30 02:38:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import RunningAlice from "./components/runningAlice/RunningAlice";
+import ParentComp from "./components/runningAlice/RunningAlice";
 
 function App() {
   return (
     <div className="App">
-      <RunningAlice />
+      <ParentComp />
     </div>
   );
 }

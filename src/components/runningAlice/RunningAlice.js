d098447b2372f70b56d8f7aad92e271759d7f77d
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-04-29 15:10:30 +0500

    installed web animation lib

--- src/components/runningAlice/RunningAlice.js
+++ src/components/runningAlice/RunningAlice.js
@@ -1,88 +1,28 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import useWebAnimations from "@wellyshen/use-web-animations";
 import "./RunningAlice.css";
 
-const RunningAlice = () => {
+const ParentComp = () => {
+  const { ref, playState } = useWebAnimations({
+    keyframes: {
+      transform: "rotate(0) translate3D(-50%, -50%, 0)",
+      color: "#000",
+      color: "#431236",
+      offset: 0.3,
+      transform: "rotate(360deg) translate3D(-50%, -50%, 0)",
+      color: "#000",
+    },
+    animationOptions: {
+      duration: 15000,
+      iteration: Infinity,
+    }
+  });
+
   return (
     <div>
-      <div className="wrapper">
-        <div className="sky"></div>
-        <div className="earth">
-          <div id="red-queen_and_alice">
-            <img
-              id="red-queen_and_alice_sprite"
-              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
-              srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
-              alt="Alice and the Red Queen running to stay in place."
-            />
-          </div>
-        </div>
-
-        <div className="scenery" id="foreground1">
-          <img
-            id="palm3"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"
-            alt="palmImage"
-          />
-        </div>
-        <div className="scenery" id="foreground2">
-          <img
-            id="bush"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x"
-            alt="palmImage"
-          />
-          <img
-            id="w_rook_upright"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x"
-            alt="palmImage"
-          />
-        </div>
-        <div className="scenery" id="background1">
-          <img
-            id="r_pawn_upright"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x"
-            alt="palmImage"
-          />
-          <img
-            id="w_rook"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x"
-            alt="palmImage"
-          />
-          <img
-            id="palm1"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x"
-            alt="palmImage"
-          />
-        </div>
-        <div className="scenery" id="background2">
-          <img
-            id="r_pawn"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x"
-            alt="palmImage"
-          />
-
-          <img
-            id="r_knight"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x"
-            alt="palmImage"
-          />
-          <img
-            id="palm2"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png"
-            srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"
-            alt="palmImage"
-          />
-        </div>
-      </div>
+      <div ref={ref} className="mainDiv"></div>
     </div>
   );
 };
 
-export default RunningAlice;
+export default ParentComp;
